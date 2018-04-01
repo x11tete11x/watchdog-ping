ea81cfdd4502769b4784df6eb7e069ef14e2d560
Tete Plaza <11tete11@gmail.com>
2018-03-31 23:14:10 -0300

    Added the destination ping to DB. It's useful to know to what ip is resolving the DNS

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ You need to create the following database:
 ```SQL
 CREATE DATABASE <DBNAME>;
 USE <DBNAME>;
-CREATE TABLE ping(id INT unsigned NOT NULL AUTO_INCREMENT, status VARCHAR(10), tx INT UNSIGNED, rx INT UNSIGNED, percent_lost INT UNSIGNED, min FLOAT, avg FLOAT, max FLOAT, mdev FLOAT, PRIMARY KEY (id),ts timestamp default current_timestamp);
+CREATE TABLE ping(id INT unsigned NOT NULL AUTO_INCREMENT, ip VARCHAR(15), status VARCHAR(10), tx INT UNSIGNED, rx INT UNSIGNED, percent_lost INT UNSIGNED, min FLOAT, avg FLOAT, max FLOAT, mdev FLOAT, PRIMARY KEY (id),ts timestamp default current_timestamp);
 ```
 2) If you need mail notify, you need to configure it and replace it in the code of the script
 
--- watchdog-ping.sh
+++ watchdog-ping.sh
@@ -7,6 +7,7 @@ DATABASE="<DATABASE NAME>"
 TRIES=4
 ############################
 PING=$(ping -qc $TRIES $IP)
+IP=$(echo -e "$PING" | grep PING | awk '{ print $3 }' | tr -d '(' | tr -d ')')
 I1=$(echo -e "$PING" | grep packet)
 TX=$(echo -e "$I1" | awk '{ print $1 }')
 RX=$(echo -e "$I1" | awk '{ print $4 }')
@@ -19,10 +20,10 @@ MAX=$(echo -e "$I2aux" | awk 'BEGIN { FS = "/" } ; { print $3 }')
 MDEV=$(echo -e "$I2aux" | awk 'BEGIN { FS = "/" } ; { print $4 }')
 if [ "$PERCENT_LOST" == "100" ]
 then
-mysql -u $USER -p$PASSW -D $DATABASE -e "insert INTO ping(status,tx,rx,percent_lost,min,avg,max,mdev) values(\"FAIL\",\"$TX\",\"$RX\",\"$PERCENT_LOST\",\"$MIN\",\"$AVG\",\"$MAX\",\"$MDEV\");"
+mysql -u $USER -p$PASSW -D $DATABASE -e "insert INTO ping(ip,status,tx,rx,percent_lost,min,avg,max,mdev) values(\"$IP\",\"FAIL\",\"$TX\",\"$RX\",\"$PERCENT_LOST\",\"$MIN\",\"$AVG\",\"$MAX\",\"$MDEV\");"
 ##### Mail notify ################### 
 #<In my script i use mailgun to send me an email, Replace this line with your method to send email>
 #####################################
 else
-mysql -u $USER -p$PASSW -D $DATABASE -e "insert INTO ping(status,tx,rx,percent_lost,min,avg,max,mdev) values(\"OK\",\"$TX\",\"$RX\",\"$PERCENT_LOST\",\"$MIN\",\"$AVG\",\"$MAX\",\"$MDEV\");"
+mysql -u $USER -p$PASSW -D $DATABASE -e "insert INTO ping(ip,status,tx,rx,percent_lost,min,avg,max,mdev) values(\"$IP\",\"OK\",\"$TX\",\"$RX\",\"$PERCENT_LOST\",\"$MIN\",\"$AVG\",\"$MAX\",\"$MDEV\");"
 fi
